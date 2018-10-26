b88166363c2c941dc8adebdec6c771192749e91e
M. ter Woord <matthijs@terwoord.eu>
2018-10-26 09:34:00 +0200

    use lld for linking

--- build.sh
+++ build.sh
@@ -61,6 +61,7 @@ if true; then
       target_cpu = "arm"
       cc = "clang-4.0"
       cxx = "clang++-4.0"
+      ld = "lld-4.0"
       
       skia_enable_gpu = false
       skia_use_libjpeg_turbo = false
